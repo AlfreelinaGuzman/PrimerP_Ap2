79f52320444c90c0fb42eb727406fd0d76b8a36f
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2021-06-02 18:41:37 -0400

    agregando contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
-//using PrimerP_Ap2.Models;
+using PrimerP_Ap2.Models;
 
 namespace PrimerP_Ap2.DAL
 {
     public class Contexto : DbContext
     {
-
+        public DbSet <Productos> Productos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Data\Productos.db");
