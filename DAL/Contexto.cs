2433c0d6b7cfcfaa7979cf62500717ecc094b264
AlfreelinaGuzman <alfreelinaguzman@gmail.com>
2021-06-02 15:14:26 -0400

    Agregando los requisitos

---
+++ DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+//using PrimerP_Ap2.Models;
+
+namespace PrimerP_Ap2.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Productos.db");
+        }
+    }
+}
